bc35b682be7ad42f33023049de740d45c67ec4d4
Protik49 <protikprotik49@gmail.com>
2025-06-09 12:29:19 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(port, () => {
+  console.log("Server running on: ", port);
+});
